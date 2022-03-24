6de19531fcb1a766411782f49a6c11d799eb1e03
Michelle Rogers <74469651+micheknows@users.noreply.github.com>
2022-03-23 20:14:10 -0600

    Begin Version 3

---
+++ src/Banking/Banking.py
@@ -0,0 +1,15 @@
+from UserInteractions import UserInteractions
+
+class Banking:
+
+    @property
+    def show_menu(self):
+        return self._show_menu
+
+    @show_menu.setter
+    def
+
+
+b = Banking()
+while b.show_menu:
+    b.display_menu()
---
+++ src/Banking/Menus.py
@@ -0,0 +1,20 @@
+class Menus:
+
+    @property
+    def menu_choices(self):
+        return self._menu_choices
+
+    @menu_choices.setter
+    def menu_choices(self, mc):
+        self._menu_choices = mc
+
+    @property
+    def menu_location(self):
+        return self._enu_location
+
+    @menu_location.setter
+    def enu_location(self, ml):
+        self._enu_location = ml
+
+
+
