dbd70eecf86f8bb39a15e5708891f356c3663b1c
Michelle Rogers <74469651+micheknows@users.noreply.github.com>
2022-03-27 13:40:27 -0600

    Pytest

---
+++ tests/test_Account.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.append("../src/Banking")
+
+from Account import Account
+
+
+def test_account():
+    """ invalid customer id assigned at creation"""
+    a = Account(3, 105, 'checking', 21.32)
+    assert str(a) == "\n\n**********\n\nACCOUNT ID:  3\nCustomer:  My Name \nAccount Type:  checking\nBalance:  $21.32"
